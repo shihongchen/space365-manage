e38a2bb1ddd4c803bb8fcbc734047e0d4167b5b2
shihongchen <88976891+shihongchen@users.noreply.github.com>
2022-04-13 02:49:40 +0800

    Add files via upload

--- user-list/src/view/UserList/index.js
+++ user-list/src/view/UserList/index.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react'
 import { Table, Button, Modal, Form, Input, message,Radio } from 'antd';
 import { nanoid } from 'nanoid';
 import moment from 'moment';
+import { ExclamationCircleOutlined} from '@ant-design/icons';
 import 'moment/locale/zh-cn';
 import './index.css'
 moment.locale('zh-cn')
@@ -58,10 +59,23 @@ export default () => {
             key: 'dateNow',
             dataIndex: 'dateNow',
           },
+          {
+            title: '操作',
+            dataIndex: '',
+            key: 'x',
+            render: (text, record, index) => (
+                <div>
+                     <Button type="link"  onClick={()=>editHanlder(record)}>编辑</Button>
+                     <Button type="link" onClick={()=>delHanlder(record.key)}>删除</Button>
+                </div>
+            ),
+          },
       ];
    
 
     const [data,setData] = useState()
+    const [isEdit,setIsEdit] = useState(false)
+    const [k,setK] = useState('')
 
     const [visible, setVisible] = useState(false);
 
@@ -74,10 +88,22 @@ export default () => {
     const [form] = Form.useForm();
   
     const onFinish = values => {
-        sessionStorage.setItem('dataList', JSON.stringify([{dateNow:moment().fromNow(),key:nanoid(),...values}, ...data]))
-        message.success('添加成功');
+        if(isEdit){
+            let localList = JSON.parse(sessionStorage.getItem('dataList'))
+            let newData = localList.reduce((res, el) => {
+                if(el.key === k) {
+                    el = {...el,...values}
+                }
+                return [...res, el];
+            }, []);
+            sessionStorage.setItem('dataList',JSON.stringify(newData))
+            setData(newData)
+        }else{
+            sessionStorage.setItem('dataList', JSON.stringify([{dateNow:moment().fromNow(),key:nanoid(),...values}, ...data]))
+        }
         form.resetFields();
         setVisible(false);
+        message.success('操作成功');
     };
     const onReset = () => {
         form.resetFields();
@@ -85,15 +111,43 @@ export default () => {
     };
     useEffect(() => {
         let localList = sessionStorage.getItem('dataList')
-        console.log(JSON.parse(localList),'123');
         localList ?  setData([...JSON.parse(localList)]) : setData([]);
     },[visible])
-  
+    const delHanlder = (id)=>{
+        Modal.confirm({
+            title: 'Confirm',
+            icon: <ExclamationCircleOutlined />,
+            content: '是否确定删除?',
+            okText: '确认',
+            cancelText: '取消',
+            onOk:()=>{
+                let localList = JSON.parse(sessionStorage.getItem('dataList'))
+                let newData = localList.filter(item=>item.key !== id )
+                sessionStorage.setItem('dataList',JSON.stringify(newData))
+                setData(newData)
+                message.error('删除成功');
+            }
+        })
+    }
+    const editHanlder = (item)=>{
+        
+        setVisible(true);
+        setK(item.key)
+        form.setFieldsValue({
+            ...item
+        });
+        setIsEdit(true)
+
+    }
     return (
         <>
+        
             <div className="userTitleBox" >
                 <h2>人员管理</h2>
-                <Button onClick={()=>setVisible(true)} type="primary">+ 添加人员</Button>
+                <Button onClick={()=>{
+                    setVisible(true)
+                     setIsEdit(false)}
+            } type="primary">+ 添加人员</Button>
              </div>
             <Table columns={columns} dataSource={data} />
             <Modal
@@ -128,11 +182,8 @@ export default () => {
                         </Radio.Group>
                     </Form.Item>
                     <Form.Item className='btnGroup' {...tailLayout}>
-                        <Button type="primary" style={{marginRight:'10px'}} htmlType="submit">确认</Button>
+                         <Button type="primary" style={{marginRight:'10px'}} htmlType="submit">确认</Button>
                         <Button htmlType="button" onClick={onReset}>取消</Button>
-                        {/* <Button type="link" htmlType="button" onClick={onFill}>
-                        Fill form
-                        </Button> */}
                     </Form.Item>
                     </Form>
             </Modal>
